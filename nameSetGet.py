d76fc84cf708dddccf1019ed146932ec3101839d
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-10-17 15:11:54 +0500

    add script 1

---
+++ nameSetGet.py
@@ -0,0 +1,13 @@
+print('Hello! This is my first python script! \\(-_- )/')
+print('What is your name?')
+name = input()
+
+print('Hah! Very well done, ' + name)
+
+# print(len(name))
+#print(name * 3)
+
+print('Now tell me your age, please')
+age = input()
+
+print('So you will be ' + str(int(age) + 1) + ' in a year! |(-.-)|')
