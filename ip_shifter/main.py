c32e2bf19e712a51afb5583b3801403bb0d4e181
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-10-20 15:31:19 +0500

    add todo

--- ip_shifter/main.py
+++ ip_shifter/main.py
@@ -1,11 +1,5 @@
 """ IP Changer Module """
 
-import os
-import sys
-import shutil
-import random
-import pyperclip
-
 # Welcome message
 print('\nHello!\n')
 print('   ;')
@@ -14,9 +8,13 @@ print('\\(o.o)')
 print('  ) (\\')
 print('  / \\\n\n')
 
+# Import scheduler functions
+SCHEDULER = __import__('scheduler')
 # Import utility functions
 UTILITIES = __import__('util')
 # Collect system NICs
 NIC_CONFIGS = UTILITIES.get_nic_list()
 # Get target NIC from user
 SELECTED_NIC = UTILITIES.get_nic_input(NIC_CONFIGS)
+# start the process
+SCHEDULER.start()
--- ip_shifter/scheduler.py
+++ ip_shifter/scheduler.py
@@ -1,8 +1,27 @@
 ''' IP scheduler '''
 
+import os
+import sys
+import shutil
+import random
+import pyperclip
+
+# Import utility functions
+UTILITIES = __import__('util')
+
+
+def start():
+    ''' Starting the IP shifting... '''
+    print('start')
+    # TODO get config.txt
+    # TODO prepare range of ip address list
+    # TODO step 1: change IP address
+    # TODO step 2: wait 5 secs
+    # TODO step 3: check connection, return to step 1 if not working
+
 
 def ip_shifter(nic):
-    ''' Initilize ipshifting mechanism on provided NIC '''
+    ''' Initilize ip shifting mechanism on provided NIC '''
 
     # IP address, subnetmask and gateway values should be unicode objects
     ip_address = u'192.168.0.11'
